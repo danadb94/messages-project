3707a666b3160857e15086342935e9e2fcc57a89
Dana biton <danadb94@gmail.com>
2021-08-30 00:00:26 +0300

    updated settings file

--- messaging_system/settings.py
+++ messaging_system/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zq^wl5fhp+w(=x==zv1ticxq&rl&ufl8wgzwn&1)7qq#p&783n'
-
+#SECRET_KEY = 'django-insecure-zq^wl5fhp+w(=x==zv1ticxq&rl&ufl8wgzwn&1)7qq#p&783n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["messaging-system-dana.herokuapp.com"]
 
 
 # Application definition
